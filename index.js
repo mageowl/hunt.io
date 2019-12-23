6e0856024c43189d58934d21edd9ef729690049d
seattleowl <seattleowl@users.noreply.github.com>
2019-12-23 08:57:39 -0800

    Fix problems

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/client/index.html")
+    res.sendFile(__dirname + "client/index.html")
 })
 
 app.listen()
