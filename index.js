3512ac30a512b347277a35dc97d6bf6b31cf5e28
seattleowl <seattleowl@users.noreply.github.com>
2019-12-23 13:08:30 -0800

    test

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "client/index.html")
+    res.sendFile(__dirname + "/client/index.html")
 })
 
-app.listen()
+app.listen(PORT)
